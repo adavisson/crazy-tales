87f71d33d463a28299e7627140825a325c0ac5c4
Andrew Davisson <akdavisson4@gmail.com>
2019-12-02 22:06:16 -0700

    new build

---
+++ build/precache-manifest.1d1c9d464364ee0857e8423bd0f97006.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "600fa90350eaeeea1f28a5bb749e4a54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32de8e53fefc478307fd",
+    "url": "/static/css/2.1c29a7ee.chunk.css"
+  },
+  {
+    "revision": "532d6f3f9e4d36ce3ff7",
+    "url": "/static/css/main.69a7394c.chunk.css"
+  },
+  {
+    "revision": "32de8e53fefc478307fd",
+    "url": "/static/js/2.2a701df4.chunk.js"
+  },
+  {
+    "revision": "532d6f3f9e4d36ce3ff7",
+    "url": "/static/js/main.6a55c9c3.chunk.js"
+  },
+  {
+    "revision": "4a1b5d0aba53c009f940",
+    "url": "/static/js/runtime-main.12bd4745.js"
+  }
+]);
