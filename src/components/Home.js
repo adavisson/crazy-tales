1a29724a017cbdbde5cc683c96c75058b9b1a77b
Andrew Davisson <akdavisson4@gmail.com>
2019-12-06 09:26:23 -0700

    Added flex for homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <>
+    <div className="home">
       <h1>Crazy Tales</h1>
       <div className="story">
         <p>Welcome to Crazy Tales! Click <a href="/templates">here</a> to get started, or if you would like to read more about this project then click <a href="/about">here</a> to go to the about page.</p>
       </div>
-    </>
+    </div>
   );
 }
  
