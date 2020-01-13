647ba0df8a7e70d87c73fc1286a1178adfabf154
Andrew Davisson <akdavisson4@gmail.com>
2020-01-13 16:10:14 -0700

    updated conditionals and removed axios

--- src/components/Story.js
+++ src/components/Story.js
@@ -3,23 +3,20 @@ import React from 'react';
 const Story = (props) => {
   
   const renderStory = () => {
-    // console.log(props.template);
-    // console.log(props.values);
     return(
       <>
         {props.template.value.map((string, index) => {
-          console.log(string);
-          if(string != 0 && props.values[index]){
+          if(string !== 0 && props.values[index]){
             return(
-              `${string}` + `${props.values[index]}`
+              string  + props.values[index]
             )
           } else if (!string && props.values[index]){
             return(
-              `${props.values[index]}`
+              props.values[index]
             )
-          } else if (string != 0){
+          } else if (string !== 0){
             return(
-              `${string}`
+              string
             )
           }
         })}
--- src/components/Templates.js
+++ src/components/Templates.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
-import axios from 'axios';
 import Story from './Story';
 
 const Templates = () => {
